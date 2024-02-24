ed3f2f1ec1f2640cf6999cb75bd66d5d4b2dc456
Sumi <sumi.tharayil@gmail.com>
2024-02-24 23:03:46 +0000

    Implement game over functionality and start new game button

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let randomNumber = Math.floor(Math.random()*100)+1
+let randomNumber = Math.floor(Math.random() * 100) + 1
 
 let guesses = document.querySelector(".guesses")
 let lastResult = document.querySelector(".lastResult")
@@ -6,6 +6,7 @@ let loworHi = document.querySelector(".loworHi")
 let guess = document.querySelector(".guess")
 let submit = document.querySelector(".submit")
 
+guess.value = ''
 guess.focus()
 let guessCount = 1;
 let resetButton;
@@ -13,16 +14,48 @@ let resetButton;
 function checkGuess() {
     const userGuess = Number(guess.value);
     console.log(userGuess)
-    if(guessCount=== 1) {
-    guesses.textContent = "Previous guesses:"
+    if (guessCount === 1) {
+        guesses.textContent = "Previous guesses:"
     }
     guesses.textContent += userGuess + ' ';
+    if (userGuess === randomNumber) {
+        lastResult.textContent = "Congradulations Buddy You got it right"
+        lastResult.style.backgroundColor = "green";
+        loworHi = ""
+        gameOver()
+
+    } else if (guessCount === 10) {
+        lastResult.textContent = "Game Over!!"
+        gameOver()
+    } else {
+        lastResult.textContent = "Wrong!"
+        lastResult.style.backgroundColor = "red";
+    }
+    if (guessCount < randomNumber) {
+        loworHi.textContent = "Guess was too low"
+    } else {
+        loworHi.textContent = "Guess was too high"
+
+    }
+
+    guessCount++;
+    guess.value = '';
+    guess.focus();
+
+}
+function gameOver() {
+    guess.disabled = true;
+    submit.disabled = true;
+    resetButton = document.createElement("button")
+    resetButton.textContent = "Start new game"
+    document.body.appendChild(resetButton)
+    resetButton.addEventListener('click', resetGame)
 }
 // 
 
 
 
 
-submit.addEventListener('click',checkGuess)
+submit.addEventListener('click', checkGuess)
 //  console.log(userGuess);
 
